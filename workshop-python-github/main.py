c6f3cb3e3ff3c7762d71efc3036516024a5db9d2
evandrini <lobatoevandro@gmail.com>
2023-06-06 21:47:42 -0300

    criei uma nova variavel para idade

--- workshop-python-github/main.py
+++ workshop-python-github/main.py
@@ -1,6 +1,7 @@
 name = input('Digite seu nome: ')
 city = input('Digite sua cidade: ')
 gender = input('Digite seu gênero: ')
+age = input("Digite sua idade")
 
 print('Seguem os dados: ')
 print(name, city, gender)
