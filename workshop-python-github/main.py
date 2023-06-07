e4ab6200c087ad201304d52080c8c924b2a58b2f
evandrini <lobatoevandro@gmail.com>
2023-06-06 21:42:21 -0300

    altera a mensagem do print

--- workshop-python-github/main.py
+++ workshop-python-github/main.py
@@ -2,5 +2,5 @@ name = input('Digite seu nome: ')
 city = input('Digite sua cidade: ')
 gender = input('Digite seu gênero: ')
 
-print('Seus dados são: ')
+print('Seguem os dados: ')
 print(name, city, gender)
